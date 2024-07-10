2116b1372db6db5726b2a77935a12909a162d99b
enicky <enicky80@gmail.com>
2024-07-10 10:56:48 +0200

    configure logging

--- python/app.py
+++ python/app.py
@@ -17,10 +17,38 @@ from utils.downloaddata import BlobRelatedClass
 from flask_dapr.app import DaprApp
 from dapr.clients import DaprClient
 from hyper_parameter_optimizer.optimizer import HyperParameterOptimizer
+from logging.config import dictConfig
+
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default",
+            },
+            "time-rotate": {
+                "class": "logging.handlers.TimedRotatingFileHandler",
+                "filename": "flask.log",
+                "when": "D",
+                "interval": 10,
+                "backupCount": 5,
+                "formatter": "default",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["console", "time-rotate"]},
+    }
+)
 
 
 app = flask.Flask(__name__)
-app.logger.setLevel(logging.DEBUG)
 dapr_app = DaprApp(app)
 
 #CORS(app)
